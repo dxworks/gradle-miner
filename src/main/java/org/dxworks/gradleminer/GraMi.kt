b81634cd1849d7e337ba4a900661dbb3c364b855
Andy <molinandy96@gmail.com>
2021-06-05 18:15:46 +0300

    Created il-deps output file

--- src/main/java/org/dxworks/gradleminer/GraMi.kt
+++ src/main/java/org/dxworks/gradleminer/GraMi.kt
@@ -33,15 +33,21 @@ fun main(args: Array<String>) {
 
     val gradleProjects = buildFiles.mapNotNull { buildFile ->
         try {
-            val nodes = astBuilder.buildFromString(CompilePhase.CONVERSION, buildFile.readText())
+            println(buildFile)
+            val content = buildFile.readText()
+            if(content.isNotBlank()) {
+                val nodes = astBuilder.buildFromString(CompilePhase.CONVERSION, content)
 
-            val visitor = DependenciesVisitor(buildFile)
+                val visitor = DependenciesVisitor(buildFile)
 
-            for (n in nodes) {
-                n.visit(visitor)
-            }
+                for (n in nodes) {
+                    try {
+                        n.visit(visitor)
+                    } catch (_: RuntimeException) {}
+                }
 
-            visitor.toGradleProject()
+                visitor.toGradleProject()
+            } else null
         } catch (e: CompilationFailedException) {
             println("Could not read build file $buildFile")
             e.printStackTrace()
@@ -53,7 +59,7 @@ fun main(args: Array<String>) {
     resultsPath.toFile().mkdirs()
 
     val modelPath = Path.of("results", "gradle-model.json")
-    val inspectorLibPath = Path.of("results", "il-deps.json")
+    val inspectorLibPath = Path.of("results", "il-gradle-deps.json")
 
     println("Writing Results...")
 
@@ -63,10 +69,8 @@ fun main(args: Array<String>) {
 
     println("Exporting Inspector Lib results to $inspectorLibPath")
     jacksonObjectMapper().writerWithDefaultPrettyPrinter().writeValue(inspectorLibPath.toFile(), gradleProjects
-        .map { it.dependencies }
-        .flatten()
-        .map { it.toInspectorLibDependency() }
-        .distinct())
+        .associate { it.relativePath to it.dependencies.map { it.toInspectorLibDependency() } }
+    )
 
     println("\nGraMi (Gradle Miner) finished successfully! Please view your results in the ./results directory")
 
@@ -79,13 +83,13 @@ data class GradleProject(
     var dependencies: List<GradleDependency> = ArrayList(),
 ) {
     @JsonProperty("path")
-    var relativePath = path?.relativeTo(baseFolder)
+    var relativePath = path?.relativeTo(baseFolder)?.parent?.replace("\\", "/")
 }
 
 class GradleProjectId(
     val group: String?,
     val name: String?,
-    val version: String?,
+    val version: String?
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
